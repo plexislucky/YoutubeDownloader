77ff6df4703f85830e1b00f62a56c2d436706587
plex <109311001+plexislucky@users.noreply.github.com>
2022-10-09 12:12:06 +0300

    Add files via upload

---
+++ YoutubeDownloader/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace YoutubeDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videourl_textbox = new System.Windows.Forms.TextBox();
+            this.downmp4_button = new System.Windows.Forms.Button();
+            this.downmp3_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // videourl_textbox
+            // 
+            this.videourl_textbox.Location = new System.Drawing.Point(13, 12);
+            this.videourl_textbox.Name = "videourl_textbox";
+            this.videourl_textbox.Size = new System.Drawing.Size(287, 20);
+            this.videourl_textbox.TabIndex = 0;
+            // 
+            // downmp4_button
+            // 
+            this.downmp4_button.Location = new System.Drawing.Point(159, 38);
+            this.downmp4_button.Name = "downmp4_button";
+            this.downmp4_button.Size = new System.Drawing.Size(141, 23);
+            this.downmp4_button.TabIndex = 1;
+            this.downmp4_button.Text = "Download MP4";
+            this.downmp4_button.UseVisualStyleBackColor = true;
+            this.downmp4_button.Click += new System.EventHandler(this.downmp4_button_Click);
+            // 
+            // downmp3_button
+            // 
+            this.downmp3_button.Location = new System.Drawing.Point(12, 38);
+            this.downmp3_button.Name = "downmp3_button";
+            this.downmp3_button.Size = new System.Drawing.Size(141, 23);
+            this.downmp3_button.TabIndex = 2;
+            this.downmp3_button.Text = "Download MP3";
+            this.downmp3_button.UseVisualStyleBackColor = true;
+            this.downmp3_button.Click += new System.EventHandler(this.downmp3_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 71);
+            this.Controls.Add(this.downmp3_button);
+            this.Controls.Add(this.downmp4_button);
+            this.Controls.Add(this.videourl_textbox);
+            this.Name = "Form1";
+            this.Text = "Youtube Video Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox videourl_textbox;
+        private System.Windows.Forms.Button downmp4_button;
+        private System.Windows.Forms.Button downmp3_button;
+    }
+}
+
---
+++ YoutubeDownloader/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using VideoLibrary;
+using NReco.VideoConverter;
+
+namespace YoutubeDownloader
+{
+    public partial class Form1 : Form
+    {
+        public string save_path = $@"C:\Users\{Environment.UserName}\Downloads\";
+        public YouTube youtube = YouTube.Default    ;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void downmp3_button_Click(object sender, EventArgs e)
+        {
+            downloadAudio();
+        }
+
+        private void downmp4_button_Click(object sender, EventArgs e)
+        {
+            downloadVideo();
+        }
+
+        public void AskFile()
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+            }
+        }
+        public void downloadVideo()
+        {
+            try
+            {
+                YouTubeVideo video = youtube.GetVideo(videourl_textbox.Text);
+                File.WriteAllBytes(save_path + video.FullName, video.GetBytes());
+                MessageBox.Show($"Video successfully downloaded!", "Success!", MessageBoxButtons.OK);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception occurred", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void downloadAudio()
+        {
+            try
+            {
+                YouTubeVideo video = youtube.GetVideo(videourl_textbox.Text);
+                File.WriteAllBytes(save_path + video.FullName, video.GetBytes());
+                FFMpegConverter convert = new FFMpegConverter();
+
+                string video_path = save_path + video.FullName;
+                string audio_path = video_path.Remove(video_path.Length - 1, 1) + "3"; // changes extension to mp3
+
+                convert.ConvertMedia(video_path, audio_path, "mp3");
+
+                File.Delete(video_path);
+                MessageBox.Show("Audio successfully downloaded!", "Success!", MessageBoxButtons.OK);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception occurred", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
